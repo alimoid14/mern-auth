01d2b7bb9f98e41e0ed30082b37ea08a10ac6073
Syed Ali Moid Rizvi <syedalimoidrizvi@gmail.com>
2024-08-23 12:04:26 +0530

    Resolving paths in production

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(cookieParser()); //It allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
